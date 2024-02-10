5703143d500ab2792d43bf07c8c0ead8bb1eae92
Vaibhav-C-S <123824888+Vaibhav-C-S@users.noreply.github.com>
2024-02-10 14:27:47 +0530

    Create preorderIterative.py

---
+++ trees/traversals/preorderIterative.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        preorder = []
+        stack = []
+        if root is None:
+            return []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+        return preorder
+
+        
+        
+
+            
+        
+        
+
