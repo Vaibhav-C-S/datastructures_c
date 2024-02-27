6453e525161eb0b8f44718dbe623b2e921fddedd
Vaibhav-C-S <123824888+Vaibhav-C-S@users.noreply.github.com>
2024-02-27 18:02:35 +0530

    Create SearchIterative.py

---
+++ BST/SearchIterative.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        temp = root
+        while temp:
+            if temp.val == val:
+                return temp
+            if val < temp.val:  
+                temp = temp.left
+            else: #val > temp.val
+                temp = temp.right
+        return None
+        
