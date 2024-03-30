5f8c5e0e924444d41a8cbfc765567b500f06d442
Vaibhav-C-S <123824888+Vaibhav-C-S@users.noreply.github.com>
2024-03-30 06:53:13 +0530

    Create OddEven.py

---
+++ LinkedList/Problems/OddEven.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None: 
+            return head
+        odd = head
+        even = head.next
+        ehead = even
+        while even != None and even.next != None:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd=odd.next
+            even= even.next
+        odd.next = ehead
+        return head
+#time complexity is o(n) and space complexity is o(1),the brute force approach is to use odd head and even head,but this approach increase space complexity
